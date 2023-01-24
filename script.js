9d192dfeed7d1b09360e37fbaf41350ffce5468b
Kristina Subjakova <ki10kikka10@gmail.com>
2023-01-24 19:24:01 +0100

    Finish console game

--- script.js
+++ script.js
@@ -1,42 +1,87 @@
-function getComputerChoice() {
-    const choice = Math.floor(Math.random() * 3);
-    if (choice === 0){
-        return "Rock";
-    } else if (choice === 1){
-        return "Paper";
+function getComputerChoice(){
+    let choiceNum = Math.floor(Math.random()*3);
+    const choices = ["Rock", "Paper","Scissors"];
+    return choices[choiceNum];
+}
+
+function getUserChoice(){
+    let input = prompt();
+    // make input case insensitive and return it
+    return input[0].toUpperCase() + input.slice(1).toLowerCase();
+}
+
+
+function playRound(){
+    let com_choice = getComputerChoice();
+    let user_choice = getUserChoice();
+    console.log(com_choice);
+    console.log(user_choice);
+
+    let result = 0;
+
+
+    if (user_choice == "Rock"){
+        if (com_choice == "Rock"){
+            result = 0;
+        } else if (com_choice == "Paper"){
+            result = -1;
+        } else {
+            result = 1;
+        }
+    } else if (user_choice == "Paper"){
+        if (com_choice == "Paper"){
+            result = 0;
+        } else if (com_choice == "Scissors"){
+            result = -1;
+        } else {
+            result = 1;
+        }
+    } else if (user_choice == "Scissors"){
+        if (com_choice == "Scissors"){
+            result = 0;
+        } else if (com_choice == "Rock"){
+            result = -1;
+        } else {
+            result = 1;
+        }
+    }
+
+    if (result == 1){
+        console.log(`You Won! ${user_choice} beats ${com_choice}`);
+    } else if (result == -1){
+        console.log(`You Lost! ${com_choice} beats ${user_choice}`);
     } else{
-        return "Scissors";
+        console.log(`It's a Draw!`);
     }
-}
-// console.log(getComputerChoice())
 
-function playRound(playerSelection, computerSelection){
-    let playerInput = playerSelection.toLowerCase();
-    let compInput = computerSelection.toLowerCase();
-    // console.log(playerInput, compInput);
-    if (playerInput === compInput){
-        return "Draw";
-    } else if (playerInput === "rock" && compInput === "scissors"){
-        return "You Win! rock beats scissors";
-    } else if (playerInput === "paper" && compInput === "rock"){
-        return "You Win! paper beats rock";
-    } else if (playerInput === "scissors" && compInput == "paper"){
-        return "You Win! scissors beat paper";
-    } else {
-        return `You Lose! ${compInput} beats ${playerInput}`;
-    }
+    return result;
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
 
 function game(){
-    let playerComp = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let userInput = prompt("Type your choice");
-        let result = playRound(userInput, getComputerChoice());
+    let com_points = 0;
+    let user_points = 0;
 
+    for (i = 0; i < 5; i++){
+        let result = playRound();
+        if (result == 1){
+            user_points += 1;
+        } else if (result == -1){
+            com_points += 1;
+        }
+        console.log(com_points);
+        console.log(user_points);
     }
+    if (user_points < com_points){
+        console.log("YOU LOSE :(((");
+    } else if (user_points > com_points){
+        console.log("YOU WIN!!!!");
+    } else {
+        console.log("It's a DRAW! :)");
+    }
+    return;
 }
+
+
+
+game();
